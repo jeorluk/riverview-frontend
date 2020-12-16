6e7884ddcf276e13d455e307b2e1dfe770b24939
Jim <jim.orluk@gmail.com>
2020-12-16 12:34:50 -0500

    Fixed appearance of links in ArtistDetail view

--- components/ArtistDetail.js
+++ components/ArtistDetail.js
@@ -6,7 +6,8 @@ import Image from 'next/image'
 
 const ArtistStyles = styled.div`
   a {
-    padding: 1rem;
+    color: ${(props) => props.theme.color.darkAccent};
+    font-weight: bold;
   }
 `
 
@@ -15,58 +16,58 @@ const ArtistCard = styled.div`
   padding: 0;
 
   img {
-    display:block;
-    margin:auto;
+    display: block;
+    margin: auto;
     max-width: 100%;
   }
-  
+
   #artistImage {
     display: block;
     max-width: 80%;
     margin: auto;
     transition: opacity 1s ease-in;
-    border-radius: 50%; 
+    border-radius: 50%;
 
     @media (min-width: ${(props) => props.theme.tabletBreak}) {
       margin-right: 60px;
       float: left;
       shape-outside: circle();
-    } 
+    }
   }
 `
 
 const ArtistImage = styled(Image)`
-div {
-
-        display: block;
-height: 200px;
-width:200px;
-background-color: purple;
+  div {
+    display: block;
+    height: 200px;
+    width: 200px;
+    background-color: purple;
     margin: auto;
     transition: opacity 1s ease-in;
-    border-radius: 50%; 
+    border-radius: 50%;
 
     @media (min-width: ${(props) => props.theme.tabletBreak}) {
       margin-right: 60px;
       float: left;
       shape-outside: circle();
       overflow: hidden;
-    } 
-}
+    }
+  }
 `
 
 const ArtistDetail = (props) => {
-    const { name, bio = '', image, featured, instruments, _id } = props
+  const { name, bio = '', image, featured, instruments, _id } = props
   const builder = useImageBuilder()
   return (
     <ArtistStyles>
       <h1>{name}</h1>
 
       <ArtistCard>
-        <img id="artistImage"
-            src={builder.image(image).height(200).width(200).url()}
-            alt={name}
-          />
+        <img
+          id='artistImage'
+          src={builder.image(image).height(200).width(200).url()}
+          alt={name}
+        />
         <div className='details'>
           {bio && <BlockContent blocks={bio} />}
           {instruments &&
