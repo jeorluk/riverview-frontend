a60ed7285908cbaaeb867f63c51bb4461489ab6f
Jim Orluk <jim.orluk@gmail.com>
2023-11-02 13:48:54 -0400

    Minor package updates and corrected warning for Sanity client after update.

--- client.js
+++ client.js
@@ -4,4 +4,5 @@ export default sanityClient({
   projectId: 'zr5hl73i', // you can find this in sanity.json
   dataset: 'production', // or the name you chose in step 1
   useCdn: true, // `false` if you want to ensure fresh data
+  apiVersion: '2023-11-02',
 })
